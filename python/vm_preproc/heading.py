FIX: Misc bug cleanup: changed imports in __init__.py to include heading, removed vmp from utils in heading.py, fixed assert statement in skewness computation

python/vm_preproc/heading.py
@@ -1,4 +1,5 @@
 import numpy as np
+from . import utils
 
 def bin_heading_simple(angles, n=12, remove_nans=True):
     """Bin a heading (in degrees) into `n` bins
@@ -18,7 +19,7 @@ def bin_heading_simple(angles, n=12, remove_nans=True):
     # Define bin centers
     abins = np.linspace(-180, 180, n, endpoint=False)
     # Compute distance to each heading bin
-    ang_dist = np.abs(np.degrees(vmp.utils.circ_dist(np.radians(st), np.radians(abins))))
+    ang_dist = np.abs(np.degrees(utils.circ_dist(np.radians(angles), np.radians(abins))))
     # soft histogram
     out = np.maximum(0, bin_width - ang_dist) / bin_width
     if remove_nans:

python/vm_preproc/image.py
@@ -49,7 +49,7 @@ def image_skewness(S, ):
         luminance image, time x vertical x horizontal
         for now, MUST be luminace image.
     """
-    assert np.dim(S) == 3 else 'Must be time x luminance image'
+    assert np.dim(S) == 3, 'Must be time x luminance image'
     n = S.shape[0]
     skewness = np.zeros((n,))
     for s in S: