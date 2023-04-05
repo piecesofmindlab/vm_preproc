ENH: image spatial downsampling & gradients, normal gradients fixed, progress bar in batch_run not mandatory for function API

vm_preproc/heading.py
@@ -30,9 +30,54 @@ def bin_heading_simple(angles, n=12, remove_nans=True):
         out[np.isnan(out)] = 0
     return out
 
+def bin_relative_heading_difference(angles,
+                                    bins=5,
+                                    temporal_block_size=30,
+                                    output_hz=0.5,
+                                    remove_nans=True):
+    """Bin a heading (in degrees) into `n` bins
+
+    Parameters
+    ----------
+    angles : array
+        t x 1 array of heading angles in degrees
+    bins : array-like or scalar int
+        bins or number of bins. if int is given, 
+
+    Notes
+    -----
+    Currently only evenly spaced bins
+    """
+    from .utils import circ_dist
+    from .image import spatial_downsample
+    if not isinstance(bins, (list, tuple, np.ndarray)):
+        bin_width = 180 / (bins - 1)
+        # Define bin centers
+        bins = np.linspace(-90, 90, bins, endpoint=True)
+
+    dif = [np.degrees(-circ_dist(np.radians(a), np.radians(b)))
+           for a, b in zip(angles[:-1, 0], angles[1:, 0])]
+    dif = np.asarray([0] + dif)
+    dif_ds = spatial_downsample(dif, block_size=(
+        temporal_block_size,), func_name='numpy.nansum', flatten=False)
+    # Compute distance to each heading bin
+    ang_dist = np.asarray(
+        [np.degrees(circ_dist(np.radians(x), np.radians(bins))) for x in dif_ds])
+    # soft histogram
+    out = np.maximum(0, bin_width - np.abs(ang_dist)) / bin_width
+    if remove_nans:
+        # remove nans
+        out[np.isnan(out)] = 0
+    return out
 
 # Visualization
-def plot_heading(angle, image=None, figsize=(8, 6), ccol=(0.8, 0.8, 0.8), acol='yellow', ax=None):
+def plot_heading(angle, image=None,
+                 figsize=(8, 6),
+                 circle_col=(0.8, 0.8, 0.8),
+                 circle_lw=2,
+                 angle_col='steelblue',
+                 angle_lw=2,
+                 ax=None):
 
     import matplotlib.pyplot as plt
 
@@ -49,14 +94,25 @@ def plot_heading(angle, image=None, figsize=(8, 6), ccol=(0.8, 0.8, 0.8), acol='
     ax.view_init(azim=90, elev=30)
     cpos = plot_utils.circle_pos(1.0, 32)
     cpos = np.vstack([cpos, cpos[0]])
-    ax.plot(*cpos.T, np.zeros((len(cpos),)), '-', color=ccol)
-    ax.plot([0, np.cos(angle + np.pi/2)], [0, np.sin(angle+np.pi/2)], [0, 0], color=acol)
+    ax.plot(*cpos.T, np.zeros((len(cpos),)), '-', color=circle_col, lw=circle_lw)
+    ax.plot([0, np.cos(angle + np.pi/2)], [0, np.sin(angle+np.pi/2)], [0, 0], color=angle_col, lw=angle_lw)
     ax.patch.set_alpha(0)
     ax.set_ylim([1, -1])
     ax.set_axis_off()
     return ax
 
-def make_heading_animation(angles, images=None, n_frames=None, figsize=(8, 6), ccol=(0.8, 0.8, 0.8), acol='yellow', img_ax=None, ax=None, fps=15):
+def make_heading_animation(angles, images=None,
+                           n_frames=None,
+                           figsize=(8, 6),
+                           circle_col=(0.8, 0.8, 0.8),
+                           circle_lw=2,
+                           angle_col='yellow',
+                           angle_lw=2,
+                           img_ax=None,
+                           cmap=None,
+                           ax=None,
+                           compass_position=None,
+                           fps=15):
 
     import matplotlib.pyplot as plt
     from matplotlib import animation
@@ -71,28 +127,28 @@ def make_heading_animation(angles, images=None, n_frames=None, figsize=(8, 6), c
     interval = 1000 / fps
 
     # First set up the figure, the axis, and the plot element we want to animate
-    if (ax is None) and (img_ax is None):
+    if img_ax is None:
         fig, img_ax = plt.subplots(figsize=figsize)
-        ax = fig.add_subplot(projection='3d')
     if ax is None:
         fig = img_ax.get_figure()
-        ax = fig.add_subplot(zorder=1, position=img_ax.get_position())
-    if img_ax is None:
-        fig = ax.get_figure()
-        img_ax = fig.add_subplot(zorder=-1, position=ax.get_position())
+        if compass_position is None:
+            compass_position = img_ax.get_position()
+        ax = fig.add_subplot(zorder=1, projection='3d',position=compass_position)
     if images is None:
         img_ax.set_axis_off()
         imh = None
     else:
-        imh = img_ax.imshow(images[0])
+        imh = img_ax.imshow(images[0], cmap=cmap)
+        img_ax.set_xticks([])
+        img_ax.set_yticks([])
 
     ax.view_init(azim=90, elev=30)
     # Plot circle
     cpos = plot_utils.circle_pos(1.0, 32)
     cpos = np.vstack([cpos, cpos[0]])
-    _ = ax.plot(*cpos.T, np.zeros((len(cpos),)), '-', color=ccol)
+    _ = ax.plot(*cpos.T, np.zeros((len(cpos),)), '-', color=circle_col, lw=circle_lw)
     # Plot heading angle
-    l0, = ax.plot([0, np.cos(angles[0] + np.pi/2)], [0, np.sin(angles[0]+np.pi/2)], [0, 0], color=acol)
+    l0, = ax.plot([0, np.cos(angles[0] + np.pi/2)], [0, np.sin(angles[0]+np.pi/2)], [0, 0], color=angle_col, lw=angle_lw)
     # Tweak plot
     ax.patch.set_alpha(0)
     ax.set_ylim([1, -1])

vm_preproc/image.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.stats
 import cv2
 from skimage import color as skcol
+from .utils import get_function
 
 def convert_color(S, conversion='rgb2lab', keep_colors=False, **kwargs):
     """Wholescale color conversion of (x, y, c, t) arrays
@@ -37,9 +38,6 @@ def convert_color(S, conversion='rgb2lab', keep_colors=False, **kwargs):
     params = dict(conversion=conversion, keep_colors=keep_colors, **kwargs)
     return out, params
 
-def spatial_downsample(S, factor, dtype='same'):
-	raise NotImplementedError('Not yet!')
-
 def image_skewness(S, ):
     """Compute skewness of image
     
@@ -55,3 +53,70 @@ def image_skewness(S, ):
     for s in S:
         skewness[i] = scipy.stats.skewness(s.reshape(n, -1), axis=1)
     return skewness
+
+def spatial_downsample(S,
+                       dtype='same',
+                       method='block_reduce',
+                       flatten=True,
+                       func_name='numpy.mean',
+                       progress_bar=None,
+                       **kwargs):
+    """Spatially downsample an array of image frames"""
+    if progress_bar is None:
+        def progress_bar(x): return x
+    if method == 'block_reduce':
+        from skimage.measure import block_reduce
+        func = get_function(func_name)
+        if 'func' in kwargs:
+            raise ValueError(
+                'Please specify downsampling function with string `func_name` kwarg')
+        if 'block_size' in kwargs:
+            kwargs['block_size'] = tuple(kwargs['block_size'])
+        out = block_reduce(S, func=func, **kwargs)
+        if flatten:
+            dim = np.prod(out.shape[1:])
+            out = out.reshape(-1, dim)
+    elif method == 'opencv':
+        raise NotImplementedError('Not yet!')
+        #out = np.asarray([cv2.resize()])
+    else:
+        raise ValueError('Unknown method "%s"' % method)
+    return out
+
+def compute_gradient(S, dimensions=(1, 2), return_type='magnitude', threshold=None):
+    """Compute gradient of input array (image or stack of images)
+    
+    Defaults assume a stack of images with time as first dimension and take 
+    gradients over next two dimensions (presumably, Y and X)
+
+    Parameters
+    ----------
+    S : array
+        input image, image stack, or other array
+    dimensions : tuple, optional
+        Axes over which to compute gradient, by default (1,2)
+    return_type : str, optional
+        'magnitude' or 'orientation', by default 'magnitude'
+    threshold : None or scalar, optional
+        threshold for gradient; if set, all gradients greater than this value 
+        are set to 1, by default None
+
+    Returns
+    -------
+    output
+        gradient array, same size as input
+    """
+    # This is horrible but there's no good way to do this sort of check in numpy;
+    # see https://github.com/numpy/numpy/issues/17325
+    if not np.issubdtype(S.dtype, np.float):
+        print('converting to float')
+        S = S.astype(np.float)
+    # Compute gradients
+    gx, gy = np.gradient(S, axis=dimensions)
+    if return_type == 'magnitude':
+        out = np.sqrt(gx**2 + gy**2)
+    elif return_type == 'orientation':
+        out = np.arctan2(gx, gy)
+    if threshold is not None:
+        out = (out > threshold).astype(np.float)
+    return out

vm_preproc/normals.py
@@ -359,7 +359,7 @@ def compute_normal_gradient(normals, nonlinexp=1):
     x_grad = np.real(np.pad((dx1 + dx2) / 2, [(0, 0), (1, 1)], 'edge'))
     y_grad = np.real(np.pad((dy1 + dy2) / 2, [(1, 1), (0, 0)], 'edge'))
     # Compute magnitude and orientation of gradients
-    grad_mag = x_grad**2 + y_grad**2
+    grad_mag = np.sqrt(x_grad**2 + y_grad**2)
     grad_ori = np.arctan2(y_grad, x_grad)
 
     return grad_mag, grad_ori