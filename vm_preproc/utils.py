dce5ac7df0583ae40d229c3bd3f16f763b821af2
marklescroart <mlescroart@unr.edu>
2026-06-18 11:26:51 -0700

    updated inspect.getfullargspec

--- vm_preproc/utils.py
+++ vm_preproc/utils.py
@@ -165,7 +165,7 @@ def get_default_kwargs(fn):
     fn : function
         function for which to get kws
     """
-    kws = inspect.getargspec(fn)
+    kws = inspect.getfullargspec(fn)
     defaults = dict(zip(kws.args[-len(kws.defaults):], kws.defaults))
     return defaults
 
